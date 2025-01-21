acdd2cb07e5708e70493d31aefa28f36a30f0c96
Adam Abdulkadir <78934570+aabdulk862@users.noreply.github.com>
2025-01-21 14:14:57 -0500

    Created Spring projects

---
+++ RecommendationAPI/src/test/java/com/revature/RecommendationAPI/RecommendationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.RecommendationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
