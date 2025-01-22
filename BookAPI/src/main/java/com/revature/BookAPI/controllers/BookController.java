ed7295a856294b35007da764a46effb4b0b8710f
Adam Abdulkadir <78934570+aabdulk862@users.noreply.github.com>
2025-01-22 15:10:57 -0500

    circuit breaker

--- BookAPI/src/main/java/com/revature/BookAPI/controllers/BookController.java
+++ BookAPI/src/main/java/com/revature/BookAPI/controllers/BookController.java
@@ -1,5 +1,7 @@
 package com.revature.BookAPI.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +13,12 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/books")
 @CrossOrigin
 public class BookController {
+        @Autowired
+        private CircuitBreakerFactory<?,?> circuitBreakerFactory;
 
         //Method that doesn't interact with any other aPI
         @GetMapping
-        public ResponseEntity<String[]> getAllBooks() {
+        public ResponseEntity<?> getAllBooks() {
                 String[] books = {
                         "To Kill a Mockingbird",
                         "1984",
@@ -30,11 +34,21 @@ public class BookController {
                 return ResponseEntity.ok(books);
         }
 
+        // This method will utilize a circuit breaker
         //Method that uses RestTemplate to consumer another API
         @GetMapping("/recs")
-        public ResponseEntity<String[]> getBookRecs() {
-                RestTemplate restTemplate = new RestTemplate();
-                String[] recs = restTemplate.getForObject("http://localhost:8081/recs", String[].class);
-                return ResponseEntity.ok(recs);
+        public ResponseEntity<?> getBookRecs() {
+                return circuitBreakerFactory.create("recsAPI").run(
+                        () -> {
+                        RestTemplate restTemplate = new RestTemplate();
+                        String[] recs = restTemplate.getForObject("http://localhost:8081/recs", String[].class);
+                        return ResponseEntity.ok(recs);
+                        }, throwable -> getRecsFallback(throwable)
+                );
+        }
+        //Method that will be used if the recs API is down
+        public ResponseEntity<?> getRecsFallback(Throwable throwable) {
+                System.out.println("Fallback method called due to exception: " + throwable.getMessage());
+                return ResponseEntity.ok("Sorry couldn't get Recommendations, please try again later");
         }
 }
